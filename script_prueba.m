b72a6a9e76ea241e0b909df64c5af2f8a222d495
Kevin del Hoyo <97704555+Xkevd@users.noreply.github.com>
2022-09-26 21:10:30 -0300

    Update script_prueba.m

--- script_prueba.m
+++ script_prueba.m
@@ -1,11 +1,16 @@
+
 casos_de_prueba
 Casos=length(mat_casos(:,1))
 LargoCoincidencia=length(mat_casos(1,:))
-mat_salida=mat_casos
+mat_salida=[]
 for i=1:Casos
-  [posCoincidencias,coincidencias]=encontrarMejor(vect_ref, mat_casos(i,:))
-  vect_parecido=vect_ref(posCoincidencias:LargoCoincidencia+posCoincidencias-1)
-  mat_salida(i,:)=vect_parecido
+[posCoincidencias,coincidencias]=encontrarMejor(vect_ref, mat_casos(i,:))
+  if [posCoincidencias==-1 && coincidencias==-1] || [posCoincidencias==0 && coincidencias==0]
+    mat_salida=mat_salida
+  else
+    vect_parecido=vect_ref(posCoincidencias:LargoCoincidencia+posCoincidencias-1)
+    mat_salida=[mat_salida ; vect_parecido]
+  endif
 endfor
 
 [fila,columna]=size(mat_salida)
